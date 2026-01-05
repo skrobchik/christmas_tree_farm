b13f37a7fe51930d426276af65d1abbbf2618550
Robert Serrano Kobylyansky <robertkobylyansky@gmail.com>
2026-01-05 08:57:41 +0100

    Don't Write DIMACS for Trivial Cases

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use rustsat::{
 
 const PRESENT_SIZE: usize = 3;
 const SOLVE: bool = true;
-const WRITE_DIMACS: bool = false;
+const WRITE_DIMACS: bool = true;
 const DIMACS_DIR: &str = "christmas_tree_farm";
 
 #[derive(Debug, Clone, Default)]
@@ -175,6 +175,15 @@ fn solve(query: &Query, shapes: &[PresentShape], test_case: usize) -> bool {
 
     let total_required_presents: usize = query.present_requirements.iter().sum();
 
+    if required_volume > available_volume {
+        println!("Trivialy Unsatisfiable");
+        return false;
+    }
+    if query.rows / PRESENT_SIZE * query.cols / PRESENT_SIZE >= total_required_presents {
+        println!("Trivially Satisfiable");
+        return true;
+    }
+
     let mut shapes: Vec<PresentShape> = shapes.into();
     let num_shapes = shapes.len();
     shapes.resize(num_shapes * 4, PresentShape::default());
@@ -222,14 +231,6 @@ fn solve(query: &Query, shapes: &[PresentShape], test_case: usize) -> bool {
             .unwrap();
     }
     if SOLVE {
-        if required_volume > available_volume {
-            println!("Trivialy Unsatisfiable");
-            return false;
-        }
-        if query.rows / PRESENT_SIZE * query.cols / PRESENT_SIZE >= total_required_presents {
-            println!("Trivially Satisfiable");
-            return true;
-        }
         let mut solver = rustsat_glucose::core::Glucose::default();
         solver.add_cnf(instance.into_cnf().0).unwrap();
         if matches!(solver.solve(), Ok(SolverResult::Sat)) {
